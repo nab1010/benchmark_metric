83971690de28a77d1fa19f1624395863551ad2a1
nabang1010 <bang.na182377@sis.hust.edu.vn>
2022-05-16 23:17:55 +0700

    nabang1010 update

---
+++ convertAnno2Origin.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+def convert2original(image, bbox):
+    x, y, w, h = convert2relative(bbox)
+
+    image_h, image_w, __ = image.shape
+
+    orig_x       = int(x * image_w)
+    orig_y       = int(y * image_h)
+    orig_width   = int(w * image_w)
+    orig_height  = int(h * image_h)
+
+    bbox_converted = (orig_x, orig_y, orig_width, orig_height)
+
+    return bbox_converted
+
+
+
+
+def main():
+    gt_path = "/home/nab/Desktop/Benchmark_Metric_Object_Detection/example/gt"
+    
+
+
+
+if __name__ == '__main__':
+    main()
